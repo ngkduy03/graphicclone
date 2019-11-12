b103d31ab1903f52d2754615786867b7f58fa453
ngkduy03 <57676310+ngkduy03@users.noreply.github.com>
2019-11-13 01:27:20 +0700

    khung done diem done key done

--- Graphic/Graphic/Source.cpp
+++ Graphic/Graphic/Source.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
 using namespace std;
 
 int main()
 {
-	char screen[30][20];
-	for (int y = 0; y < 20; y++)
+	char screen[60][20];
+	int width = 60;
+	int height = 20;
+	srand(time(NULL));
+	for (int y = 0; y < height; y++)
 	{
-		for (int x = 0; x < 30; x++)
+		for (int x = 0; x < width; x++)
 		{
-			if (y >= 1 && y <= 18 && x>0 && x<29)
+			if (y > 0 && y <height-1 && x>0 && x<width-1)
 			{
 				screen[x][y] = ' ';
 			}
@@ -28,7 +34,7 @@ int main()
 							screen[x][y] = 200;
 						}
 						break;
-					case 29:
+					case 59:
 						if (y == 0)
 						{
 							screen[x][y] = 187;
@@ -39,7 +45,7 @@ int main()
 						}
 						break;
 					default:
-						screen[x][y] = 200;
+						screen[x][y] = 205;
 						break;
 					}
 				}
@@ -52,19 +58,30 @@ int main()
 	}
 
 	
-
-
-
-
-
-
-
-
-
-
-	for (int y = 0; y < 20; y++)
+	int SnakeX = rand() % 58 + 1;
+	int Snakey = rand() % 18 + 1;
+	int Snake[2] = { SnakeX,Snakey};
+	
+	for (int y = 0; y < height; y++)
 	{
-		for (int x = 0; x < 30; x++)
+		for (int x = 0; x < width; x++)
+		{
+			if (x == Snake[0] && y == Snake[1])
+			{
+				screen[x][y] = '*';
+			}
+		}
+	}
+
+
+
+
+
+
+
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
 		{
 			cout.width(1);
 			cout << screen[x][y];
---
+++ Graphic/Graphic/keyCheck.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	/*
+	int a = _getch();
+	cout << a<< endl;
+	fflush(stdin);
+	int b = _getch();
+	cout << b<< endl;
+	fflush(stdin);
+	int c = _getch();
+	cout << c<< endl;
+	fflush(stdin);
+	int d = _getch();
+	cout << d;
+	return 0;
+	*/
+	while (1)
+	{
+		system("CLS");
+		cout << "Hello" << flush; //flush forces the printing to the screen before it clears
+		system("CLS");
+		cout << "Good-Bye" << endl;
+	}
+}
+
