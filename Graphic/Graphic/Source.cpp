a35c3d1c5caffa60068df9c0ad38da3bc5fdd443
ngkduy03 <57676310+ngkduy03@users.noreply.github.com>
2019-11-14 00:58:42 +0700

    eat funtion

--- Graphic/Graphic/Source.cpp
+++ Graphic/Graphic/Source.cpp
@@ -6,10 +6,9 @@
 using namespace std;
 
 
-void CreateScreen(char screen [] [20])
+void CreateScreen(char screen [60] [20],int height,int width)
 {
-	int width = 60;
-	int height = 20;
+	
 	for (int y = 0; y < height; y++)
 	{
 		for (int x = 0; x < width; x++)
@@ -57,10 +56,9 @@ void CreateScreen(char screen [] [20])
 		}
 	}
 }
-void draw(char screen[][20])
+void draw(char screen[60][20],int height,int width)
 {
-	int width = 60;
-	int height = 20;
+	
 	for (int y = 0; y < height; y++)
 	{
 		for (int x = 0; x < width; x++)
@@ -81,20 +79,45 @@ int main()
 	char screen[60][20];
 	int width = 60;
 	int height = 20;
-	CreateScreen(screen);
+	CreateScreen(screen,height,width);
 	srand(time(NULL));
 	int x = rand() % 58 + 1;
 	int y = rand() % 28 + 1;
-	int snake[2] = { 2,2 };
-	int moving_vector[2] = { 3,3 };
+	int snake[2] = { x,y };
+	int food[2] = { rand() % 58 + 1 ,rand() % 28 + 1 };
+	int moving_vector[2] = { 0,0 };
+	while (snake[0] == food[0] && snake[1] == food[1])
+	{
+		food[0] = rand() % 58 + 1;
+		food[1] = rand() % 18 + 1;
+	}
 	while (true)
 	{
 		system("CLS");
 		//Update
 		snake[0] += moving_vector[0];
 		snake[0] %=58+1;
+		if (snake[0] < 0) snake[0] = 58 + snake[0];
 		snake[1] += moving_vector[1];
-		snake[1] %=19+1;
+		snake[1] %=18+1;
+
+
+
+
+
+
+
+
+
+
+		while (snake[0] == food[0] && snake[1] == food[1])
+		{
+			food[0] = rand() % 58 + 1;
+			food[1] = rand() % 18 + 1;
+		}
+
+
+		if (snake[1] < 0) snake[1] = 18 + snake[1];
 		for (int y = 0; y < height; y++)
 		{
 			for (int x = 0; x < width; x++)
@@ -103,6 +126,7 @@ int main()
 				{
 					screen[x][y] = ' ';
 				}
+
 			}
 		}
 		for (int y = 0; y < height; y++)
@@ -113,10 +137,54 @@ int main()
 				{
 					screen[x][y] = '*';
 				}
+				if (x == food[0] && y == food[1] && x != 0 && x != 59 && y != 0 && y != 19)
+				{
+					screen[x][y] = 'O';
+				}
+				
 			}
 		}
 		//=======================
-		draw(screen);
+		if (_kbhit())
+		{
+			int  a = _getch();
+			switch (a)
+			{
+				case 119:
+					if (moving_vector[1] != 1)
+					{
+						moving_vector[0] = 0;
+						moving_vector[1] = -1;
+
+					}
+					break;
+				case 100:
+					if (moving_vector[0] != -1)
+					{
+						moving_vector[0] = 1;
+						moving_vector[1] = 0;
+					}
+					break;
+				case 115:
+					if (moving_vector[1] != -1)
+					{
+						moving_vector[0] = 0;
+						moving_vector[1] = 1;
+					}
+					break;
+				case 97:
+					if (moving_vector[0] != 1)
+					{
+						moving_vector[0] = -1;
+						moving_vector[1] = -0;
+					}
+					break;
+
+				default:
+				break;
+			}
+		}
+		draw(screen,height,width);	
 		Sleep(500);
 	}
 
